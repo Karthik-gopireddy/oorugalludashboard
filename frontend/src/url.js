ead51678727ae7758d4dd8f1d96b6f17cd2d7a36
Karthik Gopireddy <karthikgopireddy2001@gmail.com>
2024-10-24 16:36:16 +0530

    message

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,5 +1,5 @@
-// const url="https://oorugalludashboard.onrender.com"
+const url="https://oorugalludashboard.onrender.com"
 
-const url="http://localhost:4000"
+// const url="http://localhost:4000"
 
 export default url
